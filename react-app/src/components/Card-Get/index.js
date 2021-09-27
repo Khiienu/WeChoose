7bfffa481a64bce15274773ece73991064e139a7
Khiienu <anthonyhuynh123@gmail.com>
2021-09-27 13:40:03 -0500

    started styling

--- react-app/src/components/Card-Get/index.js
+++ react-app/src/components/Card-Get/index.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { getCardsThunk } from "../../store/card";
 import { Link } from "react-router-dom";
 import CardCreator from "../Card-Add";
-
+import './index.css'
 export default function GetCard() {
     const dispatch = useDispatch();
     const sessionUser = useSelector((state) => state.session.user);
@@ -14,14 +14,18 @@ export default function GetCard() {
     }, [dispatch])
 
     return (
-        <div className="card-case">
-            {cards.map((card) => (
-                <Link key={card.id} to={`/cards/${card.id}`}
-                className="indiv-card">
-                <h1 className="cardName">{card.name}</h1>
-                </Link>
-            ))}
-            <CardCreator/>
+        <div className="page">
+            <div className="card-case">
+                {cards.map((card) => (
+                    <Link key={card.id} to={`/cards/${card.id}`}
+                        className="indiv-card">
+                        <div className="solo-card">
+                            <h1 className="cardname">{card.name}</h1>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+                <CardCreator/>
         </div>
     )
 }
