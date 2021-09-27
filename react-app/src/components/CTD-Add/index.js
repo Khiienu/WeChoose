932109a4cc6351e57c75c2d50a39c9a177a00bc8
khiienu <anthonyhuynh123@gmail.com>
2021-09-27 12:16:02 -0500

    started front end for CTD

--- react-app/src/components/CTD-Add/index.js
+++ react-app/src/components/CTD-Add/index.js
@@ -6,6 +6,8 @@ export default function AddToDeck({cardToDeck}) {
     const dispatch = useDispatch();
     const [cardId, setCardId] = useState("")
     const [deckId,setDeckId] = useState("")
+    const [showMenu, setShowMenu] = useState(false)
+    const [showForm, setShowForm] = useState(false)
     const sessionUser = useSelector((state) => state.session.user);
     const cards = useSelector((state) => Object.values(state.cards));
     const decks = useSelector((state) => Object.values(state.decks));
@@ -20,7 +22,36 @@ export default function AddToDeck({cardToDeck}) {
         setCardId("");
         setDeckId("");
     }
+
+    const openMenu = () => setShowMenu(true);
+    const closeMenu = () => {
+        setShowMenu(false)
+        setShowForm(false)
+    };
+    const openForm = () => setShowForm(true);
+    const closeForm = () => setShowForm(false);
+
     return (
-        
+    <>
+        {showMenu &&
+            <div className="song_addForm">
+                <button onClick={!showForm ? openForm : closeForm }>Add to deck</button>
+                {showForm &&
+                    <div className="">
+                        {decks?.map(deck => (
+                            <button key={deck.id}
+                                value={deck}
+                                onClick={() =>{
+                                    dispatch(addCTD({deck: deck, card: card}))
+                                    closeMenu()
+                                    }}>
+                                {deck.name}
+                            </button>
+                        ))}
+                    </div>
+                }
+            </div>
+        }   
+    </>
     )
 }
